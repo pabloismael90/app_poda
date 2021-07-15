98f3c54304b93f8ad4cffcad172059e11ac80814
Pablo Garcia Murillo <pablo.ismael90@gmail.com>
2021-07-15 14:08:23 -0600

    finalizando cambios

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
                 const Locale('es', 'ES'),
                 // const Locale('en', 'US'),
             ],
-            title: 'Material App',
+            title: 'Herramienta Cacao Poda',
             initialRoute:'home',
             routes: {
                 'home' : ( BuildContext context ) => HomePage(),
